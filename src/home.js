a7990682fd756dcfc8d3a9017474c2248c037acf
Leah Weak <leahweak@gmail.com>
2022-04-24 13:40:42 -0700

    Add profile image upload functionality

--- src/home.js
+++ src/home.js
@@ -9,9 +9,11 @@ function setUpPage() {
     document.querySelector("#profileName").innerHTML = username;
     let account = JSON.parse(localStorage.getItem(username));
     if(account.hasOwnProperty("bio")) {
-        console.log(account.bio);
         document.querySelector("#biography").innerHTML = account.bio;
     }
+    if(account.hasOwnProperty("img")) {
+        document.querySelector("#profileImg").src = account.img;
+    } 
 }
 
 function goToSearch() {
@@ -22,11 +24,14 @@ function goToSearch() {
 
 function editProfile() {
     let params = new URLSearchParams(window.location.search),username = params.get("username");
-    document.querySelector("#editProfile").innerHTML = "Done";
-    document.querySelector("#editProfile").addEventListener("click", () => {
+    let exit = document.createElement("h3");
+    exit.innerHTML = "Don't Save";
+    exit.addEventListener("click", () => {
         document.location.href = "./home.html?"+ params.toString();
         window.open(url);
-    })
+    });
+    document.querySelector("#profile").appendChild(exit);
+
     let bio = document.querySelector("#biography").innerHTML;
     document.querySelector("#biography").innerHTML = "";
     let editBio = document.createElement("textarea");
@@ -36,17 +41,38 @@ function editProfile() {
     editBio.cols = "50";
     editBio.maxLength = "100";
     editBio.value = bio;
-    let saveBio = document.createElement("input");
-    saveBio.type = "button";
-    saveBio.id = "saveBioButton";
-    saveBio.value = "Save";
-    saveBio.addEventListener("click", () => {
+
+    ////////////////////////////Upload Image
+    let label = document.createElement("label");
+    label.for = "uploadPic";
+    label.innerHTML = "Upload picture:";
+    let upload = document.createElement("input");
+    upload.type = "file";
+    upload.name = "uploadPic";
+   // upload.id = "uploadPic";
+    upload.accept = "image/*";
+    
+    upload.addEventListener("change", function() {
+        let reader = new FileReader();
+        reader.addEventListener("load", () => {
+         // let uploadedImage = reader.result;
+          document.querySelector("#profileImg").src = reader.result;
+        });
+         reader.readAsDataURL(this.files[0]);
+      });
+    ////////////////////////////////////
+
+    document.querySelector("#editProfile").innerHTML = "Save";
+    document.querySelector("#editProfile").addEventListener("click", () => {
         let account = JSON.parse(localStorage.getItem(username)); 
         account.bio = editBio.value;
+        account.img = document.querySelector("#profileImg").src;
         localStorage.setItem(username,JSON.stringify(account));
+        document.location.href = "./home.html?"+ params.toString();
+        window.open(url);
     });
-    let par = document.createElement("p");
-    par.appendChild(saveBio);
     document.querySelector("#biography").appendChild(editBio);
-    document.querySelector("#biography").appendChild(par);
+    document.querySelector("#profile").appendChild(label);
+    document.querySelector("#profile").appendChild(upload);
+
 }
